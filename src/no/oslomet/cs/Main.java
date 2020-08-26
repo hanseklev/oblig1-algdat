6cdae853652b3252a5f9ffa996c09d9eb7c804b5
hanseklev <hanseklevstad@gmail.com>
2020-08-26 21:04:17 +0200

    oppgave 5,6 draft

--- src/no/oslomet/cs/Main.java
+++ src/no/oslomet/cs/Main.java
@@ -1,8 +1,14 @@
 package no.oslomet.cs;
 
+import no.oslomet.cs.algdat.Oblig1;
+
 public class Main {
 
     public static void main(String[] args) {
 	// kjør statiske greier her
+        char[] a = {'A','B','C','D','E','F','G','H','I','J'};
+        System.out.println(a);
+        Oblig1.rotasjon(a,-2);
+        System.out.println(a);
     }
 }
